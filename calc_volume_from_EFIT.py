Fix: vol element calculation for each area element

calc_volume_from_EFIT.py
@@ -13,16 +13,21 @@ def areaTriangle(vec1, vec2):
     return abs(0.5 * np.cross(vec1, vec2))
 
 def dVolume(R_in, Z_in, R_out, Z_out, plot_flux_surface = False):
-    area = 0.
-    vol = 0
+#    area = 0.
+#    vol = 0
+    area = []
+    vol = []
     for i in range(len(R_in) - 1):
+        print(i)
         vec1 = [R_out[i] - R_in[i], Z_out[i] - Z_in[i]]
         vec2 = [R_out[i] - R_out[i + 1], Z_out[i] - Z_out[i + 1]]
         vec3 = [R_in[i + 1] - R_out[i + 1], Z_in[i + 1] - Z_out[i + 1]]
         vec4 = [R_in[i + 1] - R_in[i], Z_in[i + 1] - Z_in[i]]
-        area += areaTriangle(vec1, vec4) + areaTriangle(vec2, vec3)
+        this_area = areaTriangle(vec1, vec4) + areaTriangle(vec2, vec3)
+        area.append(this_area)
         R_center = 0.25 * (R_in[i] + R_in[i + 1] + R_out[i] + R_out[i + 1])
-        vol += area * 2. * np.pi * R_center
+        this_vol = this_area * 2. * np.pi * R_center
+        vol.append(this_vol)
         if 1 == 0:
             plt.scatter(vec1[0], vec1[1], label = 'vec1', s = 1)
             plt.scatter(vec2[0], vec2[1], label = 'vec2', s = 1)
@@ -30,6 +35,11 @@ def dVolume(R_in, Z_in, R_out, Z_out, plot_flux_surface = False):
             plt.scatter(vec4[0], vec4[1], label = 'vec4', s = 1)
             plt.legend()
             plt.show()
+    if 1 == 0:
+        plt.plot(area)
+        plt.show()
+        plt.plot(vol)
+        plt.show()
       
     if plot_flux_surface:
         plt.plot(R_in, Z_in)
@@ -39,7 +49,7 @@ def dVolume(R_in, Z_in, R_out, Z_out, plot_flux_surface = False):
         plt.ylabel('Z (m)')
 #        plt.title(EFIT_file_name+', psip ='+str(fs_psipn))
         plt.show()
-    return vol
+    return sum(vol)
     
 EFIT_file_name = sys.argv[1]
 EFITdict = read_EFIT(EFIT_file_name)
@@ -57,10 +67,14 @@ print('psipn[-1] = {}'.format(EFITdict['psipn'][-1]))
 #print(range(1, len(EFITdict['psipn'])))
 # TODO: volumn inside the first flux surface
 dV = []
-for i in range(1, len(EFITdict['psipn'] - 1)):
+ntheta = 100
+for i in range(1, len(EFITdict['psipn']) - 1):
+    print(i)
 #if 1 == 1:
 #    i = 250
-    R_in, Z_in, B_pol_in, B_tor_in, B_tot_in = BfieldsFS(EFIT_file_name, EFITdict['psipn'][i], False)
-    R_out, Z_out, B_pol_out, B_tor_out, B_tot_out = BfieldsFS(EFIT_file_name, EFITdict['psipn'][i + 1], False)
+    R_in, Z_in, B_pol_in, B_tor_in, B_tot_in = BfieldsFS(EFITdict, EFITdict['psipn'][i], 100)
+    R_out, Z_out, B_pol_out, B_tor_out, B_tot_out = BfieldsFS(EFITdict, EFITdict['psipn'][i + 1], 100)
     dV.append(dVolume(R_in, Z_in, R_out, Z_out, False))
 print(dV)
+plt.plot(dV)
+plt.show()