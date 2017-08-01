81f8ddea7ecfa295bd6c90c2ed614598afa23eab
Xing Liu <xingliu@utexas.edu>
2017-08-01 11:47:20 -0500

    Add totalV method

--- calc_volume_from_EFIT.py
+++ calc_volume_from_EFIT.py
@@ -50,33 +50,17 @@ def dVolume(R_in, Z_in, R_out, Z_out, plot_flux_surface = False):
 #        plt.title(EFIT_file_name+', psip ='+str(fs_psipn))
         plt.show()
     return sum(vol)
-    
-EFIT_file_name = sys.argv[1]
-EFITdict = read_EFIT(EFIT_file_name)
-#print list(EFITdict.keys())
-if 1 == 0:
-    plt.plot(EFITdict['psipn'], '.')
-    plt.ylabel('psip_n')
-    plt.show()
-#psip_fs = EFITdict['psipn'][1]
-#R_fs, Z_fs, B_pol, B_tor, B_tot = BfieldsFS(EFIT_file_name, psip_fs, True)
 
-print('Total number of grid points in psipn: {}'.format(len(EFITdict['psipn'])))
-print('psipn[0] = {}'.format(EFITdict['psipn'][0]))
-print('psipn[-1] = {}'.format(EFITdict['psipn'][-1]))
-#print(range(1, len(EFITdict['psipn'])))
-# TODO: volumn inside the first flux surface
-dV = []
-psipn = []
-ntheta = 128
-for i in range(len(EFITdict['psipn']) - 1):
-    print(i)
-#if 1 == 1:
-#    i = 250
-    R_in, Z_in, B_pol_in, B_tor_in, B_tot_in = BfieldsFS(EFITdict, EFITdict['psipn'][i], ntheta)
-    R_out, Z_out, B_pol_out, B_tor_out, B_tot_out = BfieldsFS(EFITdict, EFITdict['psipn'][i + 1], ntheta)
-    dV.append(dVolume(R_in, Z_in, R_out, Z_out, False))
-    psipn.append(EFITdict['psipn'][i])
-print(sum(dV))
-plt.plot(psipn, dV)
-plt.show()
+def totalV(EFIT_file_name, ntheta):
+    EFITdict = read_EFIT(EFIT_file_name)
+    dV = []
+    psipn = []
+    for i in range(len(EFITdict['psipn']) - 1):
+        print(i)
+        R_in, Z_in, B_pol_in, B_tor_in, B_tot_in = BfieldsFS(EFITdict, EFITdict['psipn'][i], ntheta)
+        R_out, Z_out, B_pol_out, B_tor_out, B_tot_out = BfieldsFS(EFITdict, EFITdict['psipn'][i + 1], ntheta)
+        dV.append(dVolume(R_in, Z_in, R_out, Z_out, False))
+        psipn.append(EFITdict['psipn'][i])
+    return sum(dV)
+    
+
