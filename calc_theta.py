8ce39cf7378bba33da09076bf355983c1edae0b4
xingliu <xingliu@utexas.edu>
2017-08-22 07:29:41 -0700

    add originalSurf method to trace a flux surface

--- calc_theta.py
+++ calc_theta.py
@@ -42,3 +42,68 @@ def theta_grid_old(R,Z,q,psirz_spl,F,psip_n,flux_surface):
         theta_fs = np.append(theta_fs,theta_fs[i]+dtheta)
         
     return theta_fs
+
+def originalSurf(efit_file_name,flux_surface,ntheta):
+
+    fs=float(flux_surface)
+
+    psip_n, Rgrid, Zgrid, F, p, ffprime, pprime, psirz, qpsi, rmag, zmag, nw, psiax, psisep = read_EFIT_file_new(efit_file_name)
+    rdim,zdim,rctr,rmin,zmid,Bctr,curr,nh = read_EFIT_parameters(efit_file_name)
+    print 'rdim = ', rdim
+    print 'rctr = ', rctr
+    print 'rmag = ', rmag
+
+    psirz = (psirz - psiax)/(psisep-psiax)
+    psirz_spl = interpolate.RectBivariateSpline(Zgrid,Rgrid,psirz)
+
+    tol = 1.0E-7
+    l_1 = (rmin+rdim-rmag)*fs*0.99
+    l_2 = (rmin+rdim-rmag)*fs*1.01
+    theta_grid = np.empty(ntheta)
+    l_grid = np.empty(ntheta)
+    R_fs = np.empty(ntheta)
+    Z_fs = np.empty(ntheta)
+
+    for i in range(ntheta):
+        theta = 2.*np.pi*i/(ntheta-1)
+        theta_grid[i] = theta
+        R_1 = rmag+l_1*np.cos(theta)
+        Z_1 = zmag-l_1*np.sin(theta)
+        psi_1 = psirz_spl(Z_1,R_1)[0][0] - fs
+        R_2 = rmag+l_2*np.cos(theta)
+        Z_2 = zmag-l_2*np.sin(theta)
+        psi_2 = psirz_spl(Z_2,R_2)[0][0] - fs
+        istep = 0
+        while abs(psi_1)>tol and abs(psi_2)>tol and istep<20:
+            this_l = l_1-psi_1*(l_1-l_2)/(psi_1-psi_2)
+	    this_R = rmag+this_l*np.cos(theta)
+	    this_Z = zmag-this_l*np.sin(theta)
+	    this_psi = psirz_spl(this_Z,this_R)[0][0] - fs
+	    psi_2 = psi_1
+	    psi_1 = this_psi
+	    l_2 = l_1
+	    l_1 = this_l
+	    istep = istep +1
+        l_grid[i] = l_1
+        R_fs[i] = rmag+l_grid[i]*np.cos(theta)
+        Z_fs[i] = zmag-l_grid[i]*np.sin(theta)
+
+    dpsidz = np.empty(len(R_fs))
+    dpsidr = np.empty(len(R_fs))
+
+    for i in range(len(R_fs)):
+        dpsidz[i] = psirz_spl(Z_fs[i],R_fs[i],0,1,0)[0][0]
+        dpsidr[i] = psirz_spl(Z_fs[i],R_fs[i],0,0,1)[0][0]
+
+    F_spl = interpolate.UnivariateSpline(psip_n,F)
+    F_fs = F_spl(fs)
+    Bt = F_fs/R_fs
+
+    MU = 4.E-7
+
+    jtor = - Rgrid*pprime - ffprime/Rgrid
+    jtor = jtor*np.pi*MU
+
+    Bp = np.sqrt(dpsidz**2+dpsidr**2)/R_fs
+
+    return zmag,R_fs,Z_fs,dpsidz,dpsidr,Bp,Bt,qpsi,jtor,psip_n
