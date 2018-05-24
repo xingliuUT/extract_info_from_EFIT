2a0f788950acd2f59fb7826c059a8ed527b56bca
Xing Liu <xingliu@utexas.edu>
2018-05-24 12:39:14 -0500

    docs: add parentheses after print

--- finite_differences_x.py
+++ finite_differences_x.py
@@ -7,18 +7,18 @@ def first_derivative(f_in,x_in):
     dx = x[1]-x[0]
     dx1 = x[2] - x[1]
     if abs(dx - dx1) > 1.E-10:
-        print dx, dx1
+        print(dx, dx1)
         sys.exit("x grid must be uniform")
     dfdx = np.empty(len(f))
     for i in range(len(f)):
-	if i == 0:
-	    dfdx[i] = (f[i+1] - f[i])/dx
-	elif i == 1 or i == len(f)-2:
-	    dfdx[i] = (f[i+1]-f[i-1])/2./dx
-	elif i == len(f)-1:
-	    dfdx[i] = (f[i] - f[i-1])/dx
-	else:
-	    dfdx[i] = (-f[i+2]+8.*f[i+1]-8.*f[i-1]+f[i-2])/12./dx
+        if i == 0:
+            dfdx[i] = (f[i+1] - f[i])/dx
+        elif i == 1 or i == len(f)-2:
+            dfdx[i] = (f[i+1]-f[i-1])/2./dx
+        elif i == len(f)-1:
+            dfdx[i] = (f[i] - f[i-1])/dx
+        else:
+            dfdx[i] = (-f[i+2]+8.*f[i+1]-8.*f[i-1]+f[i-2])/12./dx
 	
     return dfdx
 
